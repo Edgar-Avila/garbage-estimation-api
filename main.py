aa2caf2d64a845a97401b3f0276d4d345ee8c57b
Edgar-Avila <avila.agramonte.edg@gmail.com>
2024-01-26 22:03:05 -0500

    Scale properly

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from joblib import load
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 model = load('model.joblib')
+scaler = load('scaler.joblib')
 
 app = FastAPI()
 
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get('/calc-residuos')
 def calc_garbage(rural: int, urbana: int):
-    prediction = model.predict([[rural, urbana]])
+    scaled = scaler.transform([[rural, urbana]])
+    prediction = model.predict(scaled)
     return {
         'prediccion': prediction[0][0]
     }
